77089d135a2c80115890109d65bd2809e755a0f3
leonidusatiy <62097425+leonidusatiy@users.noreply.github.com>
2020-06-28 09:27:53 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,29 +1,18 @@
 import os
-import sentry_sdk
-
-from bottle import route, run
-from sentry_sdk.integrations.bottle import BottleIntegration
-
-sentry_sdk.init(
-    dsn="https://d99f829289d94ccdbe2936fb2a978e1c@o411989.ingest.sentry.io/5288035",
-    integrations=[BottleIntegration()]
-)
-
-@route('/success')
-def success():
-    return "Hello!"
-
-@route('/fail')  
-def index():  
-    raise RuntimeError("There is an error!")  
-    return  
+from bottle import Bottle, request  
+import sentry_sdk  
+from sentry_sdk.integrations.bottle import BottleIntegration  
   
-if os.environ.get("APP_LOCATION") == "heroku":
-    run(
-        host="0.0.0.0",
-        port=int(os.environ.get("PORT", 5000)),
-        server="gunicorn",
-        workers=3,
-    )
-else:
-    run(host="localhost", port=8080, debug=True)
+sentry_sdk.init(dsn=os.environ.get("SENTRY_DSN"), integrations=[BottleIntegration()])  
+  
+app = Bottle()  
+
+@app.route('/success') 
+def success():
+    return    
+
+@app.route('/fail') 
+def fail():    
+    raise RuntimeError("There is an error!")
+    
+app.run(host='localhost', port=8080)
